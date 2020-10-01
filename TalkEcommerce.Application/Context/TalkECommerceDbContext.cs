c8fd984a2c7252c28e31180f2cf5e140411a99ef
Luis Felipe <luisfelipekai@gmail.com>
2020-10-01 07:17:20 -0300

    Revert "Revert "Commit pre Talk""
    This reverts commit 88f37a48de19471ddf824a414093e901ad8cffb5.

---
+++ TalkEcommerce.Application/Context/TalkECommerceDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TalkEcommerce.Entities;
+
+namespace TalkEcommerce.Context
+{
+    public class TalkECommerceDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
